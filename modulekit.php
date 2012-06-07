f664aaf702b84c54286887deecffb7607f72eb23
Stephan Plepelits <skunk@xover.mud.at>
2012-06-07 08:14:45 +0200

    Move language library from OpenStreetBrowser to module

---
+++ modulekit.php
@@ -0,0 +1,14 @@
+<?
+$name="Language System";
+
+$id="modulekit-lang";
+
+$depend=array("hooks");
+
+$include_php=array(
+  "inc/lang.php",
+  "inc/list.php",
+);
+$include_js=array(
+  "inc/lang.js",
+);
