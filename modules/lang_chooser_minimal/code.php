01a0aa6422d6ab7d34419751d7032d3da5f09fc6
Stephan Plepelits <skunk@xover.mud.at>
2013-01-22 12:29:52 +0100

    Lang Chooser Minimal: Show only supported languages

--- modules/lang_chooser_minimal/code.php
+++ modules/lang_chooser_minimal/code.php
@@ -1,10 +1,13 @@
 <?
 function lang_chooser_minimal() {
   global $language_list;
+  global $languages;
   global $ui_lang;
   
   $ret.="<div id='lang_chooser_minimal'>\n";
-  foreach($language_list as $code=>$name) {
+  foreach($languages as $code) {
+    $name=$language_list[$code];
+
     $r ="<a href='?ui_lang=$code'";
     if($ui_lang==$code)
       $r.=" class='chosen'";
