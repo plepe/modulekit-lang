b1b4760b0555aa1b1862c4f641adca7af76a2fc8
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-08-17 18:24:28 +0200

    build_statistic: pass directory to build_statistic()

--- inc/build_statistic.php
+++ inc/build_statistic.php
@@ -1,26 +1,26 @@
 <?php
-function build_statistic () {
+function build_statistic ($path) {
   $result = array();
 
   $template = array();
-  if (file_exists('template.json')) {
-    $template = json_decode(file_get_contents('template.json'), true);
+  if (file_exists("{$path}/template.json")) {
+    $template = json_decode(file_get_contents("{$path}/template.json"), true);
   }
-  elseif (file_exists('en.json')) {
-    $template = json_decode(file_get_contents('en.json'), true);
+  elseif (file_exists("{$path}/en.json")) {
+    $template = json_decode(file_get_contents("{$path}/en.json"), true);
   }
   $template_count = sizeof(array_keys($template));
 
   $result[''] = $template_count;
 
-  $d = opendir ('.');
+  $d = opendir($path);
   while ($f = readdir($d)) {
     if (!preg_match("/^(.*)\.json$/i", $f, $m) || substr($f, 0, 1) === '.' || $f === 'package.json' || $f === 'descriptions.json' || $f === 'template.json') {
       continue;
     }
 
     $lang = $m[1];
-    $content = json_decode(file_get_contents($f), true);
+    $content = json_decode(file_get_contents("{$path}/{$f}"), true);
     $count = 0;
     foreach ($content as $k => $v) {
       if ($v === null || $v === '') {
